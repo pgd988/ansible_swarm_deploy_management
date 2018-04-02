3750af2a80c635aaff8c05a545f93dae22bc50b3
pgd988 <pgd988@gmail.com>
2018-04-02 18:08:48 +0300

    new version of managment console: LB and Swarm nodes removing was added v0.001

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 
-DIALOG=$(dialog --title "Cluster Managment" --clear --menu "Select operation" 15 50 7  1 "Configure cluster nodes" 2 "Deploy new app release"   3 "Some Case" Q "quit" 2>&1 >/dev/tty)
+DIALOG=$(dialog --title "Cluster Managment" --clear --menu "Select operation" 15 50 7  1 "Configure cluster nodes" 2 "Deploy new app release"   3 "Manage Load Balancers" Q "quit" 2>&1 >/dev/tty)
 
 clear
 #
@@ -18,8 +18,18 @@ case $DIALOG in
 	    ;;
 	    2) 
 	    DIALOG1_2=$(dialog --clear --title "Docker Swarm managment" --inputbox "Enter node IP to REMOVE from cluster:" 15 50 3>&1 1>&2 2>&3 3>&-)
-	    sed -i "/$DIALOG1_2/d" ./inventory
+	    sed -n '/linux_servers/,/EOB/p' inventory > .inv_linx
+	    sed -n '/lb/,/EOB/p' inventory > .inv_lb
+	    if test $(grep -c $DIALOG1_2 .inv_linx) -eq 0; then
 	    clear
+	    echo "IP NOT IN LINUX_SERVERS  LIST"
+	    rm -rf ./.inv_*;
+	    else
+	    sed -i "/$DIALOG1_2/d" .inv_linx &&
+	    cat .inv_lb > inventory && cat .inv_linx >> inventory
+	    rm -rf ./.inv_*
+	    clear
+	    fi
 	    ;;
 	esac
     ;;
@@ -34,8 +44,33 @@ case $DIALOG in
 	    ;;
 	esac
     ;;
-   3) 
-    echo "some case"
+   3) DIALOG3=$(dialog --clear --title "Load Balancers managment" --menu "Select operation" 15 50 7 1 "Add new Load balancer node" 2 "Remove node" 2>&1 >/dev/tty)
+	clear
+	case $DIALOG3 in
+	1)
+	DIALOG3_1=$(dialog --clear --title "Load Balancers managment" --inputbox "Enter New Load Balancer IP:" 15 50 3>&1 1>&2 2>&3 3>&-)
+	clear
+	sed -i "/lb/a $DIALOG3_1" ./inventory
+	clear
+	ansible-playbook -i inventory LB.yml
+	clear
+	;;
+	2)
+	DIALOG3_2=$(dialog --clear --title "Load Balancers managment" --inputbox "Enter Load Balancer IP to REMOVE:" 15 50 3>&1 1>&2 2>&3 3>&-)
+	sed -n '/linux_servers/,/EOB/p' inventory > .inv_linx
+	sed -n '/lb/,/EOB/p' inventory > .inv_lb
+	if test $(grep -c $DIALOG3_2 .inv_lb) -eq 0; then
+	clear
+	echo "IP NOT IN LINUX_SERVERS  LIST"
+	rm -rf ./.inv_*;
+	else
+	sed -i "/$DIALOG3_2/d" .inv_lb
+	cat .inv_lb > inventory && cat .inv_linx >> inventory
+	rm -rf ./.inv_*
+	clear
+	fi
+	;;
+	esac
     ;;
    q|Q) 
     echo "Exiting"
