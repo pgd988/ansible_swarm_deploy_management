73986e5c390647aca1ae5e0150b87291d39472ab
pgd988 <pgd988@gmail.com>
2018-04-03 18:10:45 +0300

    new managment console new version: New Inventory Edit style

--- run.sh
+++ run.sh
@@ -1,37 +1,34 @@
 #/bin/bash
 
-DIALOG=$(dialog --title "Cluster Managment" --clear --menu "Select operation" 15 50 7  1 "Configure cluster nodes" 2 "Deploy new app release"   3 "Manage Load Balancers" Q "quit" 2>&1 >/dev/tty)
+DIALOG=$(dialog --title "Cluster Managment" --clear --menu "Select operation" 15 50 7  1 "Configure Cluster Nodes" 2 "Deploy new app release" 3 "Configure Load Balancers" Q "quit" 2>&1 >/dev/tty)
 
 clear
 #
 if [ "$?" -eq "0" ]; then
 case $DIALOG in
-   1) DIALOG1=$(dialog --clear --title "Docker Swarm managment" --menu "Select operation" 15 50 7 1 "Add new node" 2 "Remove node frome cluster" 2>&1 >/dev/tty)
+   1) DIALOG1=$(dialog --clear --title "Node roles managment" --menu "Select operation" 15 50 7 1 "Deploy configuration" 2 "Edit Nodes List" 3 "Run Config Check" 2>&1 >/dev/tty)
 	clear
 	case $DIALOG1 in
 	    1) 
-	    DIALOG1_1=$(dialog --clear --title "Docker Swarm managment" --inputbox "Enter New node IP:" 15 50 3>&1 1>&2 2>&3 3>&-)
-	    sed -i "/linux_servers/a $DIALOG1_1" ./inventory
+	    dialog --title "Deploy new Swarm Custer configuration" --msgbox 'Press Enter for Swarm Custer configuration Deploing' 10 50
 	    clear
 	    ansible-playbook -i inventory sw.yml
 	    clear
 	    ;;
 	    2) 
-	    DIALOG1_2=$(dialog --clear --title "Docker Swarm managment" --inputbox "Enter node IP to REMOVE from cluster:" 15 50 3>&1 1>&2 2>&3 3>&-)
-	    sed -n '/linux_servers/,/EOB/p' inventory > .inv_linx
-	    sed -n '/lb/,/EOB/p' inventory > .inv_lb
-	    if test $(grep -c $DIALOG1_2 .inv_linx) -eq 0; then
+	    dialog --clear --title "EDIT: LB=Load Balancers Linux_servers=Swarm Nodes" --editbox ./inventory 17 50 2>.inventory.tmp
 	    clear
-	    dialog --title 'Swarm Managment' --msgbox 'IP NOT IN LINUX_SERVERS  LIST' 10 50
-	    rm -rf ./.inv_*;
+	    cat .inventory.tmp > inventory
+	    rm -rf ./.*.tmp
 	    clear
-	    else
-	    sed -i "/$DIALOG1_2/d" .inv_linx &&
-	    cat .inv_lb > inventory && cat .inv_linx >> inventory
-	    rm -rf ./.inv_*
-	    clear
-	    fi
 	    ;;
+	    3) 
+	    dialog --title "Check new Swarm Custer configuration" --msgbox 'Press Enter for Start Checking' 10 50
+	    clear
+	    ansible-playbook -i inventory --check sw.yml
+	    clear
+	    ;;
+
 	esac
     ;;
    2) DIALOG2=$(dialog --clear --title "Deploying releases" --menu "Select operation" 15 50 7 1 "Deploy new release" 2 "Rollback release" 2>&1 >/dev/tty)
@@ -45,33 +42,29 @@ case $DIALOG in
 	    ;;
 	esac
     ;;
-   3) DIALOG3=$(dialog --clear --title "Load Balancers managment" --menu "Select operation" 15 50 7 1 "Add new Load balancer node" 2 "Remove node" 2>&1 >/dev/tty)
+   3) DIALOG3=$(dialog --clear --title "Load Balancers managment" --menu "Select operation" 15 50 7 1 "Deploy new Load Balances config" 2 "Edit Nodes List" 3 "Run Config Check" 2>&1 >/dev/tty)
 	clear
 	case $DIALOG3 in
 	1)
-	DIALOG3_1=$(dialog --clear --title "Load Balancers managment" --inputbox "Enter New Load Balancer IP:" 15 50 3>&1 1>&2 2>&3 3>&-)
-	clear
-	sed -i "/lb/a $DIALOG3_1" ./inventory
+	dialog --title "Deploy new Swarm Custer configuration" --msgbox 'Press Enter for Swarm Custer configuration Deploing' 10 50
 	clear
 	ansible-playbook -i inventory LB.yml
 	clear
 	;;
 	2)
-	DIALOG3_2=$(dialog --clear --title "Load Balancers managment" --inputbox "Enter Load Balancer IP to REMOVE:" 15 50 3>&1 1>&2 2>&3 3>&-)
-	sed -n '/linux_servers/,/EOB/p' inventory > .inv_linx
-	sed -n '/lb/,/EOB/p' inventory > .inv_lb
-	if test $(grep -c $DIALOG3_2 .inv_lb) -eq 0; then
+	dialog --clear --title "EDIT: LB=Load Balancers Linux_servers=Swarm Nodes" --editbox ./inventory 17 50 2>.inventory.tmp
 	clear
-	dialog --title 'Load Balancer Managment' --msgbox 'IP NOT IN LOAD BALANCERS  LIST' 10 50
-	rm -rf ./.inv_*;
+	cat .inventory.tmp > inventory
+	rm -rf ./.*.tmp
 	clear
-	else
-	sed -i "/$DIALOG3_2/d" .inv_lb
-	cat .inv_lb > inventory && cat .inv_linx >> inventory
-	rm -rf ./.inv_*
-	clear
-	fi
 	;;
+	3)
+	dialog --title "Check new Swarm Custer configuration" --msgbox 'Press Enter for Load Balancers configuration Checking' 10 50
+	clear
+	ansible-playbook -i inventory --check LB.yml
+	clear
+	;;
+
 	esac
     ;;
    q|Q) 
